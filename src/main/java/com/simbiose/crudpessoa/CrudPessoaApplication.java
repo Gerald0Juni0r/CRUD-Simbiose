ce542162555d53f0d20c29b0079e032769707f61
Gerald0Juni0r <geraldo.gj63@gmail.com>
2024-12-07 13:01:16 -0300

    Consertando indentação

--- src/main/java/com/simbiose/crudpessoa/CrudPessoaApplication.java
+++ src/main/java/com/simbiose/crudpessoa/CrudPessoaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudPessoaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudPessoaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPessoaApplication.class, args);
+    }
+    
 }
